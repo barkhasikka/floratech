f86be4cec146aac10e4aaf843161c119b5b85ad0
barkha sikka <sikkabarkha@gmail.com>
2018-08-12 13:37:59 +0530

    Moving content to first slide

--- public/js/components/about.jsx
+++ public/js/components/about.jsx
@@ -13,7 +13,7 @@ class About extends React.Component {
 
     }
     componentDidMount(){
-        smoothScroll("about-container", 70);
+        // smoothScroll("about-container", 70);
         this.setState({selectedTab: "about-container"})
 
     }
@@ -125,66 +125,7 @@ class About extends React.Component {
                     </section>
                 </div>
                 <div>
-            <section className="company-details-container" id="about-container">
 
-                {/*<div id="triangle-bottomright"></div>*/}
-                {/*<div id="triangle-topleft1"></div>*/}
-                <div className="company-details-section">
-                    <span className="filling-line" aria-hidden="true"></span>
-                    <div className="point-general"></div>
-                    <div className="company-thumbnail">
-                        <div className="circle-section">
-                            <div className="circle">
-                                <span className="first-company-txt">WHO</span>
-                                <span className="second-company-txt">Are We?</span>
-                            </div>
-                        </div>
-                        <div className="text-section">
-                            <blockquote className="txt-alignment">
-                                Floratechno solutions is a leading software solution company located at the heart of Pune Maharashtra.
-                                We aim to become one stop destination for all IT solution. Our team is made up of Creative Developers,Young Designers, Analyst and Industry Experts.
-                                All working towards achieving the same dream of becoming the best in the industry by empowering our clients.
-                            </blockquote>
-                        </div>
-                    </div>
-
-                    <span className="filling-line-1" aria-hidden="true"></span>
-                    <div className="point-general-1"></div>
-                    <div className="company-thumbnail">
-                        <div className="circle-section">
-                            <div className="circle">
-                                <span className="first-company-txt">WHAT</span>
-                                <span className="second-company-txt">We Do?</span>
-                            </div>
-                        </div>
-                        <div className="text-section">
-                            <blockquote className="txt-alignment">
-                                We work with industry’s most advance technologies in the field of Mobile Application, Web Application, Web Designing, Digital Marketing and Corporate Branding.
-                                We provide value to our client from these technologies by providing best service in industry for their business to grow and run smoothly.
-                                We run our business with high integrity and transparency and always deliver on our promise.
-                            </blockquote>
-                        </div>
-                    </div>
-
-
-                    <div className="company-thumbnail">
-                        <div className="circle-section">
-                            <div className="circle">
-                                <span className="first-company-txt">WHY</span>
-                                <span className="second-company-txt">Choose Us?</span>
-                            </div>
-                        </div>
-                        <div className="text-section">
-                            <blockquote className="txt-alignment">
-                                We understand our client's business needs.
-                                We work with single focus in our mind which is to provide value for money to our clients.
-                                With experience in complex project handling 
-                                we are able to deliver as per client expectations on time which helps them turn their dream into reality.
-                            </blockquote>
-                        </div>
-                    </div>
-                </div>
-            </section>
 
                 <section className="company-info-container">
                     <div className="company-info-heading">
